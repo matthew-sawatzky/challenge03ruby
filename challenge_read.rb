47c4835f6286b5e6aa3a0418dbd79c97a5e5a959
MatthewSawatz <msawatzky2@rrc.ca>
2024-05-27 18:35:02 -0500

    first commit

--- challenge_read.rb
+++ challenge_read.rb
@@ -3,3 +3,7 @@ require_relative 'ar.rb'
 number_of_products = Product.count
 
 puts "There are #{number_of_products} in the products table."
+
+any_product = Product.first
+
+puts "The first/any product is #{any_product}."
